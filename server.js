a2ef57d9e69abefd90c31e5c0c12c2a837cfa492
RafaelaSilveira <rafaelacss21@gmail.com>
2019-03-14 01:05:37 -0400

    try one more time to deploy to heroku

--- server.js
+++ server.js
@@ -6,11 +6,11 @@ const app = express();
  
 // Serve only the static files form the dist directory
 // Replace the '/dist/<to_your_project_name>'
-app.use(express.static(__dirname + '/dist/echoes-player'));
+app.use(express.static('./dist/echoes-player'));
  
 app.get('*', function(req,res) {
   // Replace the '/dist/<to_your_project_name>/index.html'
-  res.sendFile(path.join(__dirname + '/dist/echoes-player/index.html'));
+  res.sendFile(path.join(__dirname, '/dist/echoes-player/index.html'));
 });
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
