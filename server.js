772466041320ff779653928b3f01169b2c95e111
RafaelaSilveira <rafaelacss21@gmail.com>
2019-03-14 01:29:16 -0400

    one more time

--- server.js
+++ server.js
@@ -13,4 +13,9 @@ app.get('*', function(req,res) {
   res.sendFile(path.join(__dirname, '/dist/echoes-player/index.html'));
 });
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 5000);
+// app.listen(process.env.PORT || 5000);
+
+var port = process.env.PORT || 3000;
+app.listen(port, "0.0.0.0", function() {
+console.log("Listening on Port 3000");
+});
